2ed4f0dc6e83527a64aab324f63b6322a0f29099
Samuel Reid <exgin3538@yahoo.com>
2020-09-12 22:35:09 -0400

    base layout

--- yodlr-react/src/App.js
+++ yodlr-react/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Routes from './Routes';
+import NavBarComp from './NavBarComp';
 
 function App() {
   return (
     <div className='App'>
+      <NavBarComp />
       <Routes />
-      <Home />
     </div>
   );
 }
